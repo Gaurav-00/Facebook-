796ed0c549f9c0c86c8411ae49bfece7bd9dd51a
Gaurav-00 <shindegaurav5551@gmail.com>
2022-04-07 23:56:24 +0530

    automation facebook

--- main.py
+++ main.py
@@ -8,10 +8,15 @@ print("Test case started")
 driver.maximize_window()
 driver.get("https://www.facebook.com/")
 #driver.find_element_by_name("login").send_keys("login") #to searh a  word in google
-driver.find_element_by_name("email").send_keys("") #to searh a  word in google
+username=input("ENter a username-:")
+password=input("ENter a password-:")
+driver.find_element_by_name("email").send_keys(username) #to searh a  word in google
 time.sleep(3)
-driver.find_element_by_name("pass").send_keys("")
+
+driver.find_element_by_name("pass").send_keys(password)
+
 time.sleep(3)
+
 driver.find_element_by_name("login").click()
 time.sleep(5)   #time is given to hold for 5 sec
 
